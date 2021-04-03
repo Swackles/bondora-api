607024ba063278877f45cd032e741d6553732d24
Swackles <Swackles@users.noreply.github.com>
2021-04-03 13:30:17 +0300

    Added equipment types controller

---
+++ bondora-api/Controllers/EquipmentTypeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Http;
+using bondora_api.Models;
+using bondora_api.Models.EquipmentType;
+
+namespace bondora_api.Controllers
+{
+    public class EquipmentTypeController : ApiController
+    {
+        public AbstractEquipmentType Get(int id)
+        {
+            switch((EquipmentTypes)id)
+            {
+                case EquipmentTypes.Regular:
+                    return new Regular();
+                case EquipmentTypes.Heavy:
+                    return new Heavy();
+                case EquipmentTypes.Specialized:
+                    return new Specialized();
+            }
+
+            return null;
+        }
+    }
+}
